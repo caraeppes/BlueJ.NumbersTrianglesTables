39bdb39e96e9d85b179aa3ed2cc9b4bd5db1a243
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-06 13:52:34 -0500

    updated methods in NumberUtilities.java to be more concise

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -1,85 +1,51 @@
 
 
-
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        
-        String answer = "";
-        
-        for(int i = 0; i < stop; i++){
-            answer = answer + i;
-        }
-        
-        return answer;
+        return getRange(0, stop);
     }
 
     public static String getRange(int start, int stop) {
-        String answer = "";
-        
-        for (int i = start; i < stop; i++){
-            answer = answer + i;
-        }
-        
-        return answer;
+        return getRange(start, stop, 1);
     }
-
-
-    public static String getRange(int start, int stop, int step) {
+    
+   public static String getRange(int start, int stop, int step) {
         String answer = "";
-        
+
         for (int i = start; i < stop; i += step){
             answer = answer + i;
         }
-        
+
         return answer;
-        
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        String answer = "";
-        int newStart = start;
-       
-        
-        if (start % 2 == 1){
-            newStart = start + 1;
+       if (start % 2 == 1){
+           start++;
         }
-        
-        for (int i = newStart; i < stop; i += 2){
-            answer = answer + i;
-        }
-        
-        return answer;
-
+      
+       return getRange(start, stop, 2);
+            
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        String answer = "";
-        int newStart = start;
-       
         if (start % 2 == 0){
-            newStart = start + 1;
+           start++;
         }
-        
-        for (int i = newStart; i < stop; i += 2){
-            answer = answer + i;
-        }
-        
-        return answer;
-
+      
+       return getRange(start, stop, 2);
+           
     }
-
 
     public static String getExponentiations(int start, int stop, int exponent) {
         String answer = "";
-        
+
         for (int i = start; i < stop + 1; i++){
             double d = Math.pow(i, exponent);
             answer = answer + (int)d;
         }
-        
-        
+
         return answer;
     }
 }
